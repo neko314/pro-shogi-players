04eb7cfa292589a1f91e55b081c03160dd724b5a
Keiko Kaneko <keiko.cda@gmail.com>
2022-07-13 19:25:21 +0900

    Fetch pro players list

---
+++ parser.rb
@@ -0,0 +1,29 @@
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'nokogiri'
+end
+
+require 'open-uri'
+require 'json'
+
+doc = Nokogiri::HTML(URI.open("https://www.shogi.or.jp/player/"))
+
+players_nodes = doc.search("div .inner")
+nodes = players_nodes.search("div .text")
+File.open("players.json", "w") do |f|
+  players = nodes.map do |node|
+    path = node.children.children.first.attributes["href"].value # path to detail
+    name = node.search(".ttl").children.children.text # name
+    title = node.search("p").last.children.text        # title, class
+    
+    {
+      name: "#{name}",
+      title: "#{title}",
+      path: "#{path}",
+    }.to_json
+  end
+    
+  f.write({players: players}.to_json)
+end
